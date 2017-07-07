3c96c3a2c45f7db873b80f93033c68257ee91ca7
Ahhhz <ozz54321@gmail.com>
2017-07-07 17:29:21 -0400

    add nav back for resume and works

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 (function() {
 
-
-
-const show = document.querySelector('.js-showWork')
-
-show.addEventListener('click',(e)=>{
-  console.log(e);
-})
-
-
-
-
-
-
+  // const work = document.querySelector('.js-showWork')
+  //
+  // work.addEventListener('click', (e) => {
+  //   const div = document.createElement('div')
+  //   div.innerHTML = `
+  //     <div>NEW CARD</div>
+  //   `
+  //   overLay(e,div)
+  // })
+  //
+  //
+  // const overLay = (e, div) => {
+  //     const {target} = e
+  //     const {classList} = target
+  //     console.log(e);
+  //     const dropCard = document.querySelector('.box-2')
+  //     dropCard.classList.add('active')
+  //     classList.add('work-open')
+  //     const workOpen = document.querySelector('.js-work-open');
+  //     workOpen.style.display = 'block';
+  //     workOpen.appendChild(div)
+  // }//END OVERLAY
 
 })();
