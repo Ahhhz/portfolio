bef416de07aeea498397fd959bab3cadea7ea1e8
Ahhhz <ozz54321@gmail.com>
2017-07-07 17:30:00 -0400

    remove js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
 (function() {
 
-  // const work = document.querySelector('.js-showWork')
-  //
-  // work.addEventListener('click', (e) => {
-  //   const div = document.createElement('div')
-  //   div.innerHTML = `
-  //     <div>NEW CARD</div>
-  //   `
-  //   overLay(e,div)
-  // })
-  //
-  //
-  // const overLay = (e, div) => {
-  //     const {target} = e
-  //     const {classList} = target
-  //     console.log(e);
-  //     const dropCard = document.querySelector('.box-2')
-  //     dropCard.classList.add('active')
-  //     classList.add('work-open')
-  //     const workOpen = document.querySelector('.js-work-open');
-  //     workOpen.style.display = 'block';
-  //     workOpen.appendChild(div)
-  // }//END OVERLAY
-
 })();
