f38207971215c936bd74efe6279e822d02860fa9
Ahhhz <ozz54321@gmail.com>
2017-07-09 21:44:44 -0400

    close screen funtion added

--- js/index.js
+++ js/index.js
@@ -4,15 +4,18 @@
 
 
   function effect (e) {
-   
        console.log(e,workMenu);
       const {target} = e
       const effect = target.getAttribute('data-effect');
-
        workMenu.classList.toggle(effect);
        workMenu.classList.toggle('js-open');
      }
 
-     showWork.addEventListener('click', effect)
+     function closeScreen (e) {
+       const {keyCode} = e
+       keyCode === 27 ? workMenu.classList.remove('js-open'): workMenu.classList.add('close') ;
+     }
 
+     showWork.addEventListener('click', effect)
+     showWork.addEventListener('keydown', closeScreen)
 })();
