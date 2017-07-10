955647b8d084cb5d12c16b287ebafc065f3f5aa5
Ahhhz <ozz54321@gmail.com>
2017-07-09 21:29:42 -0400

    changes to js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 (function() {
+  const showWork = document.querySelector('.work')
+  const workMenu = document.querySelector('.work-menu')
+
+
+  function effect (e) {
+       // to get the correct effect
+       console.log(e,workMenu);
+      const {target} = e
+      const effect = target.getAttribute('data-effect');
+       // adding the effects
+       workMenu.classList.toggle(effect);
+       workMenu.classList.toggle('js-open');
+     }
+
+     showWork.addEventListener('click', effect)
 
 })();
